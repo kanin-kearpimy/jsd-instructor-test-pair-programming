2f7e75a8c56d9cb74354531106cb9ad1be4cba80
Upond <pond_chanawin@hotmail.com>
2024-01-12 16:29:12 +0700

    first commit

---
+++ src/Style/GlobalStyles.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    h1, h2 {
+        font-family: 'Orbitron', sans-serif;
+    }
+
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
