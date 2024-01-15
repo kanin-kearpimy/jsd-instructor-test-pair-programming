4a95569f5ea798d4b7d55f8f070b51570688a668
Upond <pond_chanawin@hotmail.com>
2024-01-15 09:58:21 +0700

    Adding Dashboard.jsx..Add all component in dashboard page

---
+++ src/Style/Wrapper.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 26.875rem;
+  background: #dddddd;
+  padding: 4rem 2rem;
+`;
+
+export const Input = styled.input`
+  padding: 2rem;
+  border: 1px solid gray;
+`;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,10 @@
+import { Input } from "../Style/Wrapper";
 const Homepage = () => {
   return (
     <div>
       <h1>This is homepage again</h1>
+      <Input type="text" />
+      <Input type="password" />
     </div>
   );
 };
