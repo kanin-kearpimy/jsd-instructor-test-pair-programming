212ec0f838a474cd03c2e0350af4aef79d4b78a0
Upond <pond_chanawin@hotmail.com>
2024-01-18 15:21:29 +0700

    Adding route: Adding navbar and create route for each main component

---
+++ src/components/Setting.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Setting = () => {
+  return (
+    <main>
+      <Nav />
+    </main>
+  );
+};
+
+export default Setting;
---
+++ src/components/Stat.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+const Stat = () => {
+  return (
+    <main>
+      <Nav />
+    </main>
+  );
+};
+
+export default Stat;
