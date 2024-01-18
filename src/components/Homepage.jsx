da5914258b4d36f5371328ee08b70144c8f4ebbf
BFSupavida <F.Supavida@gmail.com>
2024-01-18 20:47:13 +0700

    add(Homepage.jsx) : add tailwind

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,12 +2,17 @@ import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
-    <div className="">
-      {/* <img src='LogoApp/Logo-nobg.png' alt="Logo"/> */}
-      <h1>LunarFit</h1>
-      <h2>Let's work out to the moon</h2>
-      <Link to="signin">Sign in</Link>
-      <Link to="signup">Sign up</Link>
+    <div className="flex flex-col h-[926px]">
+      <img 
+      className="pt-[64px] pl-[32px]"
+      src='/src/assets/images/logo-bg-none.png' 
+      alt="Logo"/>
+      <h1 className="text-[48px] font-bold flex justify-center">LunarFit</h1>
+      <h2 className="text-[24px] font-bold flex justify-center">Let's work out to the moon</h2>
+      <div className="flex flex-col items-center mt-[90px] gap-4">
+        <Link to="signin">Sign in</Link>
+        <Link to="signup">Sign up</Link>
+      </div >
     </div>
   );
 };
