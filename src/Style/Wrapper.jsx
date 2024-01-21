96c7e1ae4af21833ce42e6549a9b5e73289965ab
Upond <pond_chanawin@hotmail.com>
2024-01-21 15:30:32 +0700

    fix: fixing height of layout.

--- src/Style/Wrapper.jsx
+++ src/Style/Wrapper.jsx
@@ -4,7 +4,8 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 26.875rem;
-  height: fit-content;
+  min-height: 100vh;
+  height: 100%;
   background: #dddddd;
   padding: 4rem 2rem 0;
 `;
