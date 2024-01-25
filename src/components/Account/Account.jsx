a9f43185da58b2a6d3fc32ea363734f453cab1ab
BFSupavida <F.Supavida@gmail.com>
2024-01-25 11:52:48 +0700

    add(changeEmailandPassword.jsx) : add

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,51 +1,16 @@
-
-import { Datepicker, Accordion, FloatingLabel } from "flowbite-react";
+import { Datepicker, FloatingLabel } from "flowbite-react";
 import React, { useState } from "react";
+import Accordion from "./ChangeEmailandPassword.jsx";
+import TitleComponent from "../TitleComponent.jsx";
 
-const mockdata = {
-  email : 'current@example.com',
-  password : 'abc1234'
-}
-
-const Account = ( ) => {
-  const [currentEmail, setCurrentEmail] = useState(mockdata.email);
-  const [newEmail, setNewEmail] = useState("");
-  const [confirmNewEmail, setConfirmNewEmail] = useState("");
-
-  function handleChangeEmail() {
-    if (currentEmail && newEmail && confirmNewEmail && newEmail === confirmNewEmail) {
-      console.log(`Changing email from ${currentEmail} to ${newEmail}`);
-      setCurrentEmail(newEmail);
-      setNewEmail('');
-      setConfirmNewEmail('');
-    } else if (!newEmail || !confirmNewEmail) {
-    console.error('Please provide both new and confirm email addresses');
-  } else {
-    console.error('New email and confirm email do not match');
-  }
-}
-
-  const [currentPassword, setCurrentPassword] = useState(mockdata.password)
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState("");
-
-  function handleChangePassword() {
-    if (currentPassword && newPassword && confirmNewPassword && newPassword === confirmNewPassword) {
-      console.log(`Changing password from ${currentPassword} to ${newPassword}`);
-      setCurrentPassword(newPassword);
-      setNewPassword('');
-      setConfirmNewPassword('');
-    } else if (!newPassword || !confirmNewPassword) {
-    console.error('Please provide both new and confirm password');
-  } else {
-    console.error('New password and confirm password do not match');
-  }
-  }
-
+const Account = () => {
   return (
     <div>
+      <TitleComponent title="Account" />
       <div className="flex flex-col gap-4 input-wrapper bg-white p-4 ">
         <h2>Personal Information</h2>
+      
+
         <div className="input relative">
           <FloatingLabel
             className="text-[1.25rem]"
@@ -58,6 +23,7 @@ const Account = ( ) => {
               alt="weight-icon"
             /> */}
           </div>
+
         </div>
         <div className="input relative">
           <FloatingLabel
@@ -79,8 +45,9 @@ const Account = ( ) => {
               <img src="/src/assets/images/icon/Height-icon.png" alt="" />
             </>
 
-            <option value="male"> 
-            <img></img>Male</option>
+            <option value="male">
+              <img></img>Male
+            </option>
             <option value="female">Female</option>
             <option value="others">Others</option>
           </select>
@@ -94,66 +61,8 @@ const Account = ( ) => {
             showTodayButton={false}
           />
         </div>
-        {/* change email */}
-        <Accordion collapseAll>
-          <Accordion.Panel>
-            <Accordion.Title>Change Email</Accordion.Title>
-            <Accordion.Content>
-              <p>Current Email</p>
-              <input 
-                className="border border-black" 
-                type="email"
-                value={currentEmail}
-                onChange={(e) => setCurrentEmail(e.target.value)}
-                readOnly 
-                />
-              <p>New Email</p>
-              <input 
-                className="border border-black"
-                type="email"
-                value={newEmail}
-                onChange={(e) => setNewEmail(e.target.value)} />
-              <p>Confirm New Email</p>
-              <input 
-                className="border border-black" 
-                value={confirmNewEmail}
-                onChange={(e) => setConfirmNewEmail(e.target.value)}
-                />
-              <button
-                onClick={handleChangeEmail}>
-                  Save</button>
-            </Accordion.Content>
-          </Accordion.Panel>
-          {/* change password */}
-          <Accordion.Panel>
-            <Accordion.Title>Change Password</Accordion.Title>
-            <Accordion.Content>
-              <p>Current Password</p>
-              <input 
-                type="password" 
-                className="border border-black" 
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                readOnly />
-              <p>New Password</p>
-              <input 
-                type="password" 
-                className="border border-black" 
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}/>
-              <p>Confirm New Password</p>
-              <input 
-                type="password" 
-                className="border border-black" 
-                value={confirmNewPassword}
-                onChange={(e) => setConfirmNewPassword(e.target.value)}/>
-              <button
-                onClick={handleChangePassword}>
-                  Save</button>
-            </Accordion.Content>
-          </Accordion.Panel>
-        </Accordion>
-          {/* delete account */}
+        <Accordion />
+        {/* delete account */}
         <button
           id="delete-account"
           type="button"
